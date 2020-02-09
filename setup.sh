555e8c0360a0f2b3166eed9596e5b70b10733cae
taq-fujihara <taq.fujihara@gmail.com>
2020-02-09 17:11:55 +0900

    Samplerのインストールスクリプト
    とインストールスクリプトを実行する部分をsetup.shに追加。

--- setup.sh
+++ setup.sh
@@ -5,7 +5,16 @@ SETUP_DIR=`pwd`
 
 for f in .??*
 do
-    [[ "$f" == ".git" ]] && continue
-    ln -sv $SETUP_DIR/$f ~/
+	[[ "$f" == ".git" ]] && continue
+	[[ "$f" == *.swp ]] && continue
+
+	ln -sv $SETUP_DIR/$f ~/
+done
+
+# install packages
+for f in install/*.sh
+do
+	echo "install `basename $f .sh`"
+	$f
 done
 
