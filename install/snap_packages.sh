9fdb9f3a479cc4bf6cc31ce153cda2696a712ee0
taq-fujihara <taq.fujihara@gmail.com>
2020-04-30 10:33:55 +0900

    fix typo

--- install/snap_packages.sh
+++ install/snap_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z `which snapa` ]; then
+if [ -z `which snap` ]; then
   echo "snap command not found. abort..."
   exit
 fi
@@ -11,6 +11,10 @@ echo "read packages from $PACAKGES/snap_packages.txt"
 
 cat $PACKAGES/snap_packages.txt | while read package
 do
+  if [ -z "$package" ]; then
+    continue
+  fi
+
   echo "install $package"
   sudo snap install $package
 done
