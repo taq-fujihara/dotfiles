4d3b88d905d3c31fa55a5a36aa35a3e44087b84e
taq-fujihara <taq.fujihara@gmail.com>
2020-05-25 23:18:06 +0900

    add bash_profile

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ SETUP_DIR=`pwd`
 echo -e "\n\n====== Create Links of Dotfiles ======\n\n"
 
 ln -sfv $SETUP_DIR/home/bashrc ~/.bashrc
+ln -sfv $SETUP_DIR/home/bash_profile ~/.bash_profile
 ln -sfv $SETUP_DIR/home/gitconfig ~/.gitconfig
 ln -sfv $SETUP_DIR/home/inputrc ~/.inputrc
 ln -sfv $SETUP_DIR/home/sampler.config ~/.sampler.config
