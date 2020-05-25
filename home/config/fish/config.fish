d2d4c43065a509ce0258ab1f8578f038899dc3b5
taq-fujihara <taq.fujihara@gmail.com>
2020-05-25 20:51:48 +0900

    use ctrl+f with vim mode (fish)

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -1,6 +1,15 @@
 function fish_prompt
-    powerline-shell --shell bare $status
+  powerline-shell --shell bare $status
 end
 
 fish_vi_key_bindings
+
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+function fish_user_key_bindings
+  for mode in insert default visual
+    fish_default_key_bindings -M $mode
+  end
+  fish_vi_key_bindings --no-erase
+end
 
