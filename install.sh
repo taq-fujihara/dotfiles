f9d4241d078d3206ec7c59ea6834309a33904460
taq-fujihara <taq.fujihara@gmail.com>
2020-05-25 21:14:05 +0900

    command alias for fish (ll -> ls -alF)

--- install.sh
+++ install.sh
@@ -15,4 +15,6 @@ ln -sfv $SETUP_DIR/home/powerline-shell.json ~/.powerline-shell.json
 mkdir -p ~/.config/fish
 ln -sfv $SETUP_DIR/home/config/fish/fish_variables ~/.config/fish/fish_variables
 ln -sfv $SETUP_DIR/home/config/fish/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/functions
+ln -sfv $SETUP_DIR/home/config/fish/functions/ll.fish ~/.config/fish/functions/ll.fish
 
