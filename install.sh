a61bf7cd5cadff0e0687ee767bab48a82ce202ba
taq-fujihara <taq.fujihara@gmail.com>
2020-11-11 13:33:24 +0000

    add vim and powerline-shell install command

--- install.sh
+++ install.sh
@@ -3,6 +3,20 @@
 cd `dirname $0`
 SETUP_DIR=`pwd`
 
+if type "apt" > /dev/null 2>&1; then
+  echo "install programs via apt"
+  sudo apt install vim
+fi
+
+if type "vim" > /dev/null 2>&1; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if type "pip3" > /dev/null 2>&1; then
+  pip3 install powerline-shell
+fi
+
 echo -e "\n\n====== Create Links of Dotfiles ======\n\n"
 
 ln -sfv $SETUP_DIR/home/bashrc ~/.bashrc
