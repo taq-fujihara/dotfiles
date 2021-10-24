c6d67a327af53c4703a03b4323c91d863c5b9cde
taq-fujihara <taq.fujihara@gmail.com>
2021-10-24 22:13:33 +0900

    Add fish_pre/postexec

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -16,6 +16,22 @@ function fish_user_key_bindings
   bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
 
+function before_command --on-event fish_preexec
+  if test -d "node_modules"
+    # add node module binary path when node_modules directory.
+    # just bypass npx...
+    set -x PATH $PATH ./node_modules/.bin
+  end
+end
+
+function after_command --on-event fish_postexec
+  if set -l index (contains -i "./node_modules/.bin" $PATH)
+    # remove node module binary path if it exists.
+    # cleaning up a mess created in preexec.
+    set -e PATH[$index]
+  end
+end
+
 #################################################
 # Aliases
 #################################################
