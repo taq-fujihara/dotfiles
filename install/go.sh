95627371cfd92f1a9f7592b6e5f59107696cfd08
taq-fujihara <taq.fujihara@gmail.com>
2023-04-09 16:31:39 +0900

    modify go installation shell

--- install/go.sh
+++ install/go.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:longsleep/golang-backports
-sudo apt update
-sudo apt install -y golang
+GO_VERSION=1.20.3
+
+wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz -P /tmp
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf /tmp/go${GO_VERSION}.linux-amd64.tar.gz
+
+# add /usr/local/go/bin to PATH
+
