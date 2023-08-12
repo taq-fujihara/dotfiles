df41b555bc225db9a905621ec27b726a565e1e56
taq-fujihara <taq.fujihara@gmail.com>
2023-08-12 23:10:02 +0900

    modify astronvim user config url

--- install/astronvim.sh
+++ install/astronvim.sh
@@ -11,5 +11,5 @@ if [ -d ~/.config/nvim/lua/user ]; then
     echo '~/.config/nvim/lua/user already exists. skip installing AstroNvim user config...'
 else
     echo 'Install astronvim (user config)'
-    git clone https://github.com/taq-fujihara/astronvim_config.git ~/.config/nvim/lua/user
+    git clone git@github.com:taq-fujihara/astronvim_config.git ~/.config/nvim/lua/user
 fi
