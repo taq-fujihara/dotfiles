6ff9e9b7601a128785c999b65456791e967bd465
taq-fujihara <taq.fujihara@gmail.com>
2023-09-12 22:47:40 +0900

    add node_modules bin path
    fish_add_path didn't work...

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -12,22 +12,6 @@ function fish_user_key_bindings
   bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
 
-function before_command --on-event fish_preexec
-  if test -d "node_modules"
-    # add node module binary path when node_modules directory.
-    # just bypass npx...
-    set -x PATH $PATH ./node_modules/.bin
-  end
-end
-
-function after_command --on-event fish_postexec
-  if set -l index (contains -i "./node_modules/.bin" $PATH)
-    # remove node module binary path if it exists.
-    # cleaning up a mess created in preexec.
-    set -e PATH[$index]
-  end
-end
-
 #################################################
 # Abbreviations
 #################################################
@@ -52,6 +36,7 @@ abbr --add dexecbash --set-cursor "docker exec -it % /bin/bash"
 fish_add_path --global --move /usr/local/go/bin
 fish_add_path --global --move ~/.local/bin
 fish_add_path --global --move ~/go/bin
+set -x PATH $PATH ./node_modules/.bin
 
 #################################################
 # Environment Variables
