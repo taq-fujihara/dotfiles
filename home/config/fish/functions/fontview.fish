201f647278bbdfc66e901d5e8614baeeedc0e1c7
taq-fujihara <taq.fujihara@gmail.com>
2023-12-18 17:37:58 +0900

    add new fish function: fontview

---
+++ home/config/fish/functions/fontview.fish
@@ -0,0 +1,17 @@
+function fontview
+    printf "\
+あいうえおかきくけこZZ
+ababababababababababZZ
+
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789
+
+Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+
+全角スペース：　
+
+Ligature
+== != === !== => ->
+" | cat
+end
