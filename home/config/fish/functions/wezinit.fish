1ee848bdfe5d095941274de315f7292724aad9cd
taq-fujihara <taq.fujihara@gmail.com>
2023-12-20 16:02:42 +0900

    modify fish function: wezinit

--- home/config/fish/functions/wezinit.fish
+++ home/config/fish/functions/wezinit.fish
@@ -1,5 +1,14 @@
 function wezinit -d "Setup wezterm panes"
-  wezterm cli split-pane --left
-  wezterm cli split-pane --top
-  wezterm cli adjust-pane-size --amount 16 Right
+    set -l window_type $argv[1]
+
+    switch $window_type
+        case 'dev'
+          command wezterm cli split-pane --top
+          command wezterm cli split-pane --left
+          command wezterm cli adjust-pane-size --amount 8 Down
+        case '*'
+          command wezterm cli split-pane --left
+          command wezterm cli split-pane --top
+          command wezterm cli adjust-pane-size --amount 16 Right
+    end
 end
