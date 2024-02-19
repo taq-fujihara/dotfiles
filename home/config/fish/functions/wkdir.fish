0ec4afce62767ef68dbcdbd4cba88aca392b987b
taq-fujihara <taq.fujihara@gmail.com>
2024-02-20 08:40:01 +0900

    update "wkdir" fish function

--- home/config/fish/functions/wkdir.fish
+++ home/config/fish/functions/wkdir.fish
@@ -1,16 +1,16 @@
 function wkdir -d "Create a work directory and set CWD"
     argparse 's/suffix=' 'p/parmanent' -- $argv
 
-    set -l destination /tmp
+    set -l destination /tmp/work
     if set -lq _flag_p
         set destination $HOME/work
-        mkdir -p $destination
     end
+    mkdir -p $destination
 
     set -lq _flag_s
     or set -l _flag_s (date "+%H%M%S")
 
-    set d $destination/work_(date "+%Y-%m-%d")-$_flag_s
+    set d $destination/(date "+%Y-%m-%d")-$_flag_s
     mkdir $d
     cd $d
 end
