6a588aea7b4b4f5cf9eb9cf02d4a00839dd3a7f8
taq-fujihara <taq.fujihara@gmail.com>
2025-06-15 14:08:34 +0900

    add java lsp and debugger

--- home/config/nvim/lua/plugins/mason.lua
+++ home/config/nvim/lua/plugins/mason.lua
@@ -9,6 +9,8 @@ return {
         "debugpy",
         "deno",
         "eslint-lsp",
+        "jdtls",
+        "java-debug-adapter",
         "js-debug-adapter",
         "lua-language-server",
         "ruff",
@@ -19,34 +21,3 @@ return {
     },
   },
 }
-
---[[
-
--- Customize Mason
-
----@type LazySpec
-return {
-  -- use mason-tool-installer for automatically installing Mason packages
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- overrides `require("mason-tool-installer").setup(...)`
-    opts = {
-      -- Make sure to use the names found in `:Mason`
-      ensure_installed = {
-        -- install language servers
-        "lua-language-server",
-
-        -- install formatters
-        "stylua",
-
-        -- install debuggers
-        "debugpy",
-
-        -- install any other package
-        "tree-sitter-cli",
-      },
-    },
-  },
-}
-
-]]
