99ed5390182374105164cc527b8f72c61de65814
taq-fujihara <taq.fujihara@gmail.com>
2026-01-02 22:40:35 +0900

    set wezterm default font

--- home/config/wezterm/wezterm.lua
+++ home/config/wezterm/wezterm.lua
@@ -40,7 +40,10 @@ elseif COLOR_SCHEME == "nord" then
 end
 
 config.color_scheme = COLOR_SCHEME
+
+config.font = wezterm.font('UDEV Gothic 35NFLG')
 config.line_height = 1.1
+
 config.hide_tab_bar_if_only_one_tab = false -- I want workspace name to be always visible
 config.window_background_opacity = 0.95
 config.inactive_pane_hsb = {
