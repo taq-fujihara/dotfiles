405e7c0ae95432a37d49b3dc4f5fe928335c2609
taq-fujihara <taq.fujihara@gmail.com>
2026-06-22 08:54:56 +0900

    edit nvim color theme spec

--- home/config/nvim/lua/plugins/astroui.lua
+++ home/config/nvim/lua/plugins/astroui.lua
@@ -1,11 +1,9 @@
-local color_scheme = os.getenv("MY_COLOR_SCHEME")
-
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = color_scheme,
+    colorscheme = "nord",
   },
 }
 
