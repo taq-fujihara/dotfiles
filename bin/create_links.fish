5d2eacbcef794b62df28a4d0e34f361eec95bc88
taq-fujihara <taq.fujihara@gmail.com>
2026-07-13 21:31:13 +0900

    install hunkdiff

--- bin/create_links.fish
+++ bin/create_links.fish
@@ -89,6 +89,7 @@ create_link config/git/ignore .config/git/ignore
 create_link config/git/hooks/force-push-confirmation.fish .config/git/hooks/force-push-confirmation.fish
 create_link config/gitui/key_bindings.ron .config/gitui/key_bindings.ron
 create_link config/ghostty/config.ghostty .config/ghostty/config.ghostty
+create_link config/hunk/config.toml .config/hunk/config.toml
 create_link config/jj/config.toml .config/jj/config.toml
 # https://karabiner-elements.pqrs.org/docs/manual/misc/configuration-file-path/#about-symbolic-link
 create_link config/karabiner .config/karabiner
