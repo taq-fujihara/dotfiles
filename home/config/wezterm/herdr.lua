4d594dc4614bb642f86b2e7f5046e767d8276685
Takuya Fujihara <taq@Takuyas-MacBook-Air.local>
2026-08-08 18:46:40 +0900

    wezterm; add user-var-changed event handler

--- home/config/wezterm/herdr.lua
+++ home/config/wezterm/herdr.lua
@@ -20,6 +20,18 @@ config.keys = {
 	},
 }
 
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	wezterm.log_info("USER VAR CHANGED:", name, value)
+	if name == "wezterm_action" and value == "quickselect" then
+		window:perform_action(act.QuickSelect, pane)
+	end
+end)
+
 config.default_prog = { "herdr" }
+
+local has_override, apply_to_config = pcall(require, "override")
+if has_override then
+	apply_to_config(config)
+end
 
 return config
