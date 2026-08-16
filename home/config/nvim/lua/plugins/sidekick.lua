1b959ed38119bb597187405af29629ce8854d80c
taq-fujihara <taq.fujihara@gmail.com>
2026-08-16 09:24:10 +0900

    Activate sidekick mux backend only when `HERDR_ENV` is set to "1"

--- home/config/nvim/lua/plugins/sidekick.lua
+++ home/config/nvim/lua/plugins/sidekick.lua
@@ -4,7 +4,7 @@ return {
     cli = {
       mux = {
         backend = "herdr",
-        enabled = true,
+        enabled = vim.env.HERDR_ENV == "1",
       },
       win = {
         layout = "left",
