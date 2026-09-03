57e1c5ba53fd6570b19944f077ff233deda3664b
taq-fujihara <taq.fujihara@gmail.com>
2026-09-03 09:15:26 +0900

    Warn before mutating jj repos with git

--- home/config/fish/functions/git.fish
+++ home/config/fish/functions/git.fish
@@ -1,4 +1,114 @@
-function git -d "git wrapper that disables side-by-side diff when the terminal is narrow"
+function git -d "git command wrapper"
+    __git_wrapper_warn_jj_repo $argv
+
+    if __git_wrapper_disable_side_by_side_diff
+        command git -c delta.side-by-side=false $argv
+    else
+        command git $argv
+    end
+end
+
+function __git_wrapper_warn_jj_repo
+    if set -q GIT_WRAPPER_NO_JJ_WARN
+        return
+    end
+
+    if not __git_wrapper_may_mutate_repo $argv
+        return
+    end
+
+    set -l root (__git_wrapper_root $argv)
+
+    if test -n "$root"; and test -d "$root/.jj"
+        echo "warning: this repo has .jj; consider using jj" >&2
+    end
+end
+
+function __git_wrapper_may_mutate_repo
+    set -l subcommand (__git_wrapper_subcommand $argv)
+    set -l mutating_subcommands \
+        add \
+        branch \
+        checkout \
+        cherry-pick \
+        clean \
+        commit \
+        merge \
+        mv \
+        pull \
+        push \
+        rebase \
+        reset \
+        restore \
+        rm \
+        stash \
+        switch \
+        tag
+
+    contains -- $subcommand $mutating_subcommands
+end
+
+function __git_wrapper_subcommand
+    set -l argc (count $argv)
+    set -l i 1
+
+    while test $i -le $argc
+        set -l arg $argv[$i]
+
+        switch $arg
+            case --
+                return 1
+            case -C -c --git-dir --work-tree --namespace
+                set i (math $i + 1)
+            case '-C=*' '-c=*' '--git-dir=*' '--work-tree=*' '--namespace=*'
+            case '-*'
+            case '*'
+                echo $arg
+                return 0
+        end
+
+        set i (math $i + 1)
+    end
+
+    return 1
+end
+
+function __git_wrapper_root
+    set -l options (__git_wrapper_global_options $argv)
+
+    command git $options rev-parse --show-toplevel 2>/dev/null
+end
+
+function __git_wrapper_global_options
+    set -l argc (count $argv)
+    set -l i 1
+
+    while test $i -le $argc
+        set -l arg $argv[$i]
+
+        switch $arg
+            case --
+                return
+            case -C -c --git-dir --work-tree --namespace
+                echo $arg
+                set i (math $i + 1)
+
+                if test $i -le $argc
+                    echo $argv[$i]
+                end
+            case '-C=*' '-c=*' '--git-dir=*' '--work-tree=*' '--namespace=*'
+                echo $arg
+            case '-*'
+                echo $arg
+            case '*'
+                return
+        end
+
+        set i (math $i + 1)
+    end
+end
+
+function __git_wrapper_disable_side_by_side_diff
     set -l cols $COLUMNS
 
     if test -z "$cols"
@@ -9,9 +119,5 @@ function git -d "git wrapper that disables side-by-side diff when the terminal i
         set cols 80
     end
 
-    if test $cols -lt 160
-        command git -c delta.side-by-side=false $argv
-    else
-        command git $argv
-    end
+    test $cols -lt 160
 end
